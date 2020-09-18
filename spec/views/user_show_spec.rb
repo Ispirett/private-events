e9fe9a3e5914bc67b21760683bc988730b8bb48f
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2020-09-18 18:59:20 +0500

    Add view tests

---
+++ spec/views/user_show_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'the user show page process', type: :feature do
+  it 'sign up a new user and login to show page' do
+    visit new_user_path
+    within('#test_signup') do
+      fill_in 'user[username]', with: 'umair.ahmad'
+      fill_in 'user[first_name]', with: 'umair'
+      fill_in 'user[last_name]', with: 'ahmad'
+      fill_in 'user[email]', with: 'umair.ahmad@gmail.com'
+    end
+    click_button 'Sign Up'
+    expect(page).to have_content 'Sign Up'
+
+    visit new_session_users_path
+    expect(page).to have_content('Username')
+
+    within('#test') do
+      fill_in 'username', with: 'umair.ahmad'
+    end
+    click_button 'Sign in'
+
+    visit user_path(1)
+    expect(page).to have_content('Registered Events')
+  end
+end
